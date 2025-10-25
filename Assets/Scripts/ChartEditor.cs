31378294af57174e9dbc700718ce12e6d2a46977
Ciaran-M-97 <159147820+Ciaran-M-97@users.noreply.github.com>
2025-10-25 01:02:23 +0100

    Layout changes+health swap

---
+++ Assets/Scripts/ChartEditor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ChartEditor : MonoBehaviour
+{
+    GameObject chart;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        PrefabUtility.SaveAsPrefabAsset(chart, "Assets / Charts");
+
+        
+    }
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -14,30 +14,58 @@ public class Health : MonoBehaviour
     public float healthToAdd;
     public float healthToDeduct;
 
+    public GameObject spike;
+    Vector3 spikeNewPos;
+    public float speed;
+    public float maxX;
+    public float minX;
+    float newX;
+
 
     void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = maxHealth/2;
+        newX = minX + (maxX - minX) * (currentHealth / maxHealth);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentHealth > maxHealth)
-        {
-            currentHealth = maxHealth;
-        }
+        spikeNewPos = new Vector3(newX, spike.transform.position.y, spike.transform.position.z);
+        spike.transform.position = Vector3.MoveTowards(spike.transform.position, spikeNewPos, speed * Time.deltaTime);
+        
     }
 
     public void AddHealth()
     {
         currentHealth += healthToAdd;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
         healthBar.value = currentHealth;
+        newX = minX + (maxX - minX) * (currentHealth / maxHealth);
+        print(newX);
+        print(currentHealth);
+        
     }
     
     public void DeductHealth()
     {
         currentHealth -= healthToDeduct;
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
         healthBar.value = currentHealth;
+        newX = minX + (maxX - minX) * (currentHealth / maxHealth);
+        print(newX);
+        print(currentHealth);
     }
 }
